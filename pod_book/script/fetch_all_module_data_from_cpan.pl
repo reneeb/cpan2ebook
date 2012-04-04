8eccda7d153b68aaf262511ecc64351200574760
Boris Däppen <boris_daeppen@bluewin.ch>
2012-04-04 22:56:34 +0200

    cpan info dump works

--- pod_book/script/fetch_all_module_data_from_cpan.pl
+++ pod_book/script/fetch_all_module_data_from_cpan.pl
@@ -1,11 +1,15 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+
 use LWP::Simple;
 use File::Temp qw/ tempdir /;
 use Archive::Extract;
  
-$dir = tempdir();
-$filename = "$dir/02packages.details.txt.gz";
+my $dir = tempdir();
+my $filename = "$dir/02packages.details.txt.gz";
+my $db_file = 'cpan_names.db';
  
 my $status = getstore(
                 "http://www.cpan.org/modules/02packages.details.txt.gz",
@@ -23,30 +27,42 @@ if ( is_success($status) )
         print "unpacked\n";
 
         use DBI;
-        my $db = DBI->connect("dbi:SQLite:test.db", "", "",
+        unlink $db_file if -e $db_file;
+        my $db = DBI->connect("dbi:SQLite:$db_file", "", "",
         {RaiseError => 1, AutoCommit => 1});
-        $db->do("CREATE TABLE release (name VARCHAR(100))");
+        $db->do("CREATE TABLE names (
+                                     module  VARCHAR(200),
+                                     release VARCHAR(100)
+                                    )"
+               );
 
+        my %double_entry = ();
         open (my $f, "$dir/02packages.details.txt");
         while (<$f>) {
             if ($_ =~
                  m/^([\w:]+)\s+[\w\d\.]+\s+\w{1}\/\w{2}\/\w+\/([\w-]+)\d+/
                ) {
-                my $module = $1;
+                my $module  = $1;
                 my $release = $2;
                 chop($release);
 
-                $db->do("INSERT INTO release VALUES ('$release')");
+                unless (exists $double_entry{"$module-$release"}) {
+                    $db->do("INSERT INTO names
+                                VALUES ('$module', '$release')"
+                           );
+                    $double_entry{"$module-$release"} = 1;
+                }
             }
         }
         close ($f); 
         print "filled into DB\n";
 
-        my $all = $db->selectall_arrayref("SELECT * FROM release");
+        my $all = $db->selectall_arrayref("SELECT * FROM names");
 
+        # select release from names where module='EBook::MOBI';
         foreach my $row (@$all) {
-            my ($id) = @$row;
-            print "$id\n";
+            my ($mod, $rel) = @$row;
+            print "$mod\t$rel\n";
         }
 
         $db->disconnect;
