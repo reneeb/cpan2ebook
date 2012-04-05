8130d7ec6bba3740f21d44f1055fe76e11ed5841
reneeb <github@renee-baecker.de>
2012-04-05 17:29:56 +0200

    clean up PodBook.pm

--- pod_book/lib/PodBook.pm
+++ pod_book/lib/PodBook.pm
@@ -1,18 +1,18 @@
 package PodBook;
+
+use strict;
+use warnings;
+
 use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
   my $self = shift;
 
-  # Documentation browser under "/perldoc" (this plugin requires Perl 5.10)
-  #$self->plugin('pod_renderer');
-
   # Routes
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->route('/welcome')->to('example#welcome');
   $r->route('/')      ->to('cpan_search#form');
   $r->route('/upload')->to('upload#form');
 
