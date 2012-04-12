d7516d237d4af8bacb39d04958325d809a77697d
Boris Däppen <boris_daeppen@bluewin.ch>
2012-04-12 20:45:26 +0200

    performant updating

--- pod_book/script/fetch_all_module_data_from_cpan.pl
+++ pod_book/script/fetch_all_module_data_from_cpan.pl
@@ -6,71 +6,115 @@ use warnings;
 use LWP::Simple;
 use File::Temp qw/ tempdir /;
 use Archive::Extract;
+use DBI;
+
+$| = 1;
  
-my $dir = tempdir();
-my $filename = "$dir/02packages.details.txt.gz";
-my $db_file = 'cpan_names.db';
+my $db_file  = 'cpan_names.db';
+my $source   = "02packages.details.txt.gz";
+my $dir      = '/tmp';#tempdir();
+my $filename = "$dir/$source";
+
+print "Downloading '$source' from CPAN to '$dir'...";
+#my $status = getstore(                                               # fetch
+             #"http://www.cpan.org/modules/02packages.details.txt.gz",# from
+             #$filename                                               # to
+             #);
+print "DONE\n";
  
-my $status = getstore(
-                "http://www.cpan.org/modules/02packages.details.txt.gz",
-                $filename
-             );
- 
-if ( is_success($status) )
-{
-    print "file downloaded correctly at: $filename\n";
+#unless ( is_success($status) ) {
+    #print "FAIL\t$status\n";
+    #exit 1;
+#}
 
-    my $ae = Archive::Extract->new( archive => $filename );
-    my $ok = $ae->extract( to => $dir );
+print 'Extracting archive...';
+my $ae = Archive::Extract->new( archive => $filename );
+my $ok = $ae->extract( to => $dir );
 
-    if ($ok) {
-        print "unpacked\n";
+unless ($ok) {
+    print "FAIL\n";
+    exit 1;
+}
+print "DONE\n";
 
-        use DBI;
-        unlink $db_file if -e $db_file;
-        my $db = DBI->connect("dbi:SQLite:$db_file", "", "",
-        {RaiseError => 1, AutoCommit => 1});
-        $db->do("CREATE TABLE names (
-                                     module  VARCHAR(200),
-                                     release VARCHAR(100)
-                                    )"
-               );
+my $db = DBI->connect("dbi:SQLite:$db_file", "", "",
+    {RaiseError => 1, AutoCommit => 1});
 
-        my %double_entry = ();
-        open (my $f, "$dir/02packages.details.txt");
-        while (<$f>) {
-            if ($_ =~
-                 m/^([\w:]+)\s+[\w\d\.]+\s+\w{1}\/\w{2}\/\w+\/([\w-]+)\d+/
-               ) {
-                my $module  = $1;
-                my $release = $2;
-                chop($release);
+unless (-e $db_file) {
+    print "No DB found. Creating new DB with table in '$db_file'...";
+    $db->do("CREATE TABLE names (
+                                 module  VARCHAR(200),
+                                 release VARCHAR(100)
+                                )"
+           );
+    print "DONE\n";
+}
 
-                unless (exists $double_entry{"$module-$release"}) {
-                    $db->do("INSERT INTO names
-                                VALUES ('$module', '$release')"
-                           );
-                    $double_entry{"$module-$release"} = 1;
-                }
-            }
+print 'Dumping DB into hash...';
+my $res = $db->selectall_arrayref(
+                        "SELECT module, release FROM names");
+my %stored = ();
+foreach my $row (@$res) {
+    my ($module, $release) = @$row;
+    $stored{"$module-$release"} = 1;
+}
+print "DONE\n";
+
+print 'Updating DB...';
+my $inserted = 0;
+my $skipped  = 0;
+my $nomatch  = 0;
+my $nomatch_txt  = '';
+open (my $f, "$dir/02packages.details.txt");
+while (<$f>) {
+    if ($_ =~ m%
+                ^
+                ([\w:]+)
+                \s+
+                [\w\d\.]+
+                \s+
+                \w{1}\/\w{2}\/\w+\/
+                ([\w\d\-\.]+)
+                \.(tar\.gz|tgz|zip)
+              %x
+       ) {
+        my $module  = $1;
+        my $release = $2;
+        if ($release =~ m/(.*)-[\w\d\.]+/) {
+            $release = $1;
         }
-        close ($f); 
-        print "filled into DB\n";
+        #else {
+            #print "ERROR: $release\n";
+        #}
 
-        my $all = $db->selectall_arrayref("SELECT * FROM names");
-
-        # select release from names where module='EBook::MOBI';
-        foreach my $row (@$all) {
-            my ($mod, $rel) = @$row;
-            print "$mod\t$rel\n";
+#print $_;
+#print "$module\t$release\n";
+#next;
+        if ($stored{"$module-$release"}) {
+            $skipped++;
+            next;
         }
-
-        $db->disconnect;
-
+        else {
+            $db->do("INSERT INTO names
+                        VALUES ('$module', '$release')"
+                   );
+            $inserted++;
+        }
+    }
+    else {
+        $nomatch++;
+        $nomatch_txt .= $_;
     }
 }
-else
-{
-    print "error downloading file to $filename: $status\n";
-}
+close ($f); 
+$db->disconnect;
+print "DONE\n";
+
+$| = 0;
+print "Inserted: $inserted, Skipped: $skipped, Not matched: $nomatch\n";
+print "The following lines did not match the criteria for data:\n";
+print "--------------------------------------------------------\n";
+print $nomatch_txt;
+
+exit 0;
 
