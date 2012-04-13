446f1e0eec424fa99cd5cc8844cbb3ffbf5032f3
Boris Däppen <boris_daeppen@bluewin.ch>
2012-04-13 15:48:47 +0200

    DB location changed

--- pod_book/lib/PodBook/Utils/CPAN/Names.pm
+++ pod_book/lib/PodBook/Utils/CPAN/Names.pm
@@ -3,6 +3,8 @@ package PodBook::Utils::CPAN::Names;
 use strict;
 use warnings;
 
+use File::Basename;
+use File::Spec;
 use YAML::Tiny;
 use PodBook::Utils::CPAN::Names::Source;
 
@@ -13,8 +15,10 @@ sub new {
        ) = @_;
 
 
-    my $yaml = YAML::Tiny->new;
-    $yaml = YAML::Tiny->read( 'config.yml' );
+    my $bin_dir = File::Spec->rel2abs( dirname __FILE__ );
+    my $yaml    = YAML::Tiny->read(
+        File::Spec->catfile( $bin_dir, '..', 'config.yml' ),
+        );
     my $src_name = $yaml->[0]->{cpan_namespaces_source};
 
     my $source = PodBook::Utils::CPAN::Names::Source->new('DB',$src_name);
