3b95b42a6e4046b91a7d649deef38085c247a9ce
Boris Däppen <boris_daeppen@bluewin.ch>
2012-04-13 15:55:37 +0200

    DB location

--- pod_book/example/CPAN_names.pl
+++ pod_book/example/CPAN_names.pl
@@ -1,8 +1,22 @@
 #!/usr/bin/perl
 
+use YAML::Tiny;
+use File::Basename;
+use File::Spec;
+
 use PodBook::Utils::CPAN::Names;
 
-my $t = PodBook::Utils::CPAN::Names->new('DB', 'cpan_names.db');
+my $bin_dir = File::Spec->rel2abs( dirname __FILE__ );
+ 
+ my $yaml    = YAML::Tiny->read(
+     File::Spec->catfile( $bin_dir, '..', 'config.yml' ),
+     );
+
+  
+my $db_file  = $yaml->[0]->{cpan_namespaces_source};
+
+
+my $t = PodBook::Utils::CPAN::Names->new('DB', $db_file);
 
 print $t->translate_any2release('EBook::MOBI');
 print "\n";
