e39f71f147d61c0a708ae2308bf6d21c5876eb80
reneeb <github@renee-baecker.de>
2012-04-27 13:55:42 +0200

    get listsize from config

--- pod_book/lib/PodBook/About.pm
+++ pod_book/lib/PodBook/About.pm
@@ -9,7 +9,12 @@ sub form {
     my $self = shift;
 
     # this line is needed because otherwise "About" crashes
-    $self->stash( listsize => 1 );
+    my $listsize = $self->config->{autocompletion_size} || 10;
+    if ( $listsize =~ /\D/ or $listsize > 100 ) {
+        $listsize = 10;
+    }
+
+    $self->stash( listsize => $listsize );
 
     $self->render();
 }
