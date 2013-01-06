47f0d0bffd9ca5c120d0e6b938917f8997709ee9
reneeb <github@renee-baecker.de>
2013-01-06 23:45:29 +0100

    use MailException plugin

--- pod_book/lib/PodBook.pm
+++ pod_book/lib/PodBook.pm
@@ -32,6 +32,18 @@ sub startup {
 
   $self->plugin( 'Mail' => $config->{mail} );
 
+  $self->plugin( 'MailException' => {
+      %{ $config->{exception} || {} },
+      send => sub {
+          my ($mail,$exception) = @_;
+
+          $mail->send(
+              $config->{mail}->{how},
+              @{ $config->{mail}->{howargs} || [] },
+          );
+      },
+  });
+
   # set log level
   $self->app->log(
       Mojo::Log->new(
